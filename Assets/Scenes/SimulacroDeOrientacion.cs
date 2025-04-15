e3a37be2552b91f8b4c70f4430ac3f014c8830f4
Geraldine-Oriolo <49374998@est.ort.edu.ar>
2025-04-15 08:58:38 -0300

    Update SimulacroDeOrientacion.cs

--- Assets/Scenes/SimulacroDeOrientacion.cs
+++ Assets/Scenes/SimulacroDeOrientacion.cs
@@ -29,7 +29,7 @@ public class SimulacroDeOrientacion : MonoBehaviour
             return;
         }else
         {
-            Debug.Log(" Muchas gracias" + nombreAlumno);
+            Debug.Log(" Muchas gracias " + nombreAlumno);
         }
 
     }
