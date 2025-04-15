ef289a18a43886880848e3711f16e5ed0788bb33
Geraldine-Oriolo <49374998@est.ort.edu.ar>
2025-04-15 08:54:25 -0300

    Examen Unity simulacro

--- Assets/Scenes/SimulacroDeOrientacion.cs
+++ Assets/Scenes/SimulacroDeOrientacion.cs
@@ -14,19 +14,24 @@ public class SimulacroDeOrientacion : MonoBehaviour
         if (nombreAlumno == "")
         {
             Debug.Log("El nombre no es valido");
-        }
-        if (Curso > 5 || Curso < 1)
+            return;
+        }else  if (Curso > 5 || Curso < 1)
         {
             Debug.Log(" El curso no es valido");
+            return;
         }else if ( Curso > 3 )
         {
             Debug.Log("Error. Aún estás en el ciclo básico");
-        }
-
-        if (orientacion != "T" & orientacion != "D" & orientacion != "G" & orientacion != "M" & orientacion != "H")
+            return;
+        }else if (orientacion != "T" & orientacion != "D" & orientacion != "G" & orientacion != "M" & orientacion != "H")
         {
             Debug.Log("Solo puede ingresar T, D, G, M o H");
+            return;
+        }else
+        {
+            Debug.Log(" Muchas gracias" + nombreAlumno);
         }
+
     }
 
     // Update is called once per frame
